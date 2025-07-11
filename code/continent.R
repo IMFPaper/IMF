bc0f9332eacb87457256414afb7cf7ce24266b42
Dianyi Yang <kv9898@rubuky.com>
2025-07-11 18:40:53 +0800

    add continent to rds

---
+++ code/continent.R
@@ -0,0 +1,40 @@
+######## INFO ########
+# Authors: Dianyi Yang
+# R Script
+# Purpose: This script runs PCA and regression analysis for the demo.
+# Inputs:  data/panel_data_raw.dta
+#          data/continents.json
+# Outputs: data/panel_data.rds
+
+# SETUP ----------------------------------------------------------------------------------------------------------------
+rm(list = ls()) # Clear workspace
+
+need <- c(
+  'tidyverse',
+  'jsonlite'
+) # list packages needed
+have <- need %in% rownames(installed.packages()) # checks packages you have
+if (any(!have)) install.packages(need[!have]) # install missing packages
+invisible(lapply(need, library, character.only = T))
+
+# Read data ----------------------------------------------------------------------------------------------------------------
+raw_data <- read_dta("data/panel_data_raw.dta")
+continent_map <- fromJSON("data/continents.json")
+
+# Check coverage of countries in the map -----------------------------------------------------------------------------------------------
+continent_df <- data.frame(
+  country = names(continent_map),
+  continent = unname(unlist(continent_map))
+)
+
+country_list <- unique(raw_data$country)
+country_list[!(country_list %in% continent_df$country)]
+  ## character(0) # No missing countries
+
+# Merge data with continent map -----------------------------------------------------------------------------------------------
+data <- raw_data |> 
+  left_join(continent_df, by = "country") |> 
+  relocate(continent, .after = isocode)
+
+# Save data ------------------------------------------------------------------------------------------------------------------
+write_rds(data, "data/panel_data.rds")
