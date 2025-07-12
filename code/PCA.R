be9e59730a7ec89fed0a1a2b152d2a668b086f24
Dianyi Yang <kv9898@rubuky.com>
2025-07-12 17:35:36 +0800

    move f test generation to helper

--- code/PCA.R
+++ code/PCA.R
@@ -233,51 +233,7 @@ msummary(regModels, stars = T) # Rough regression table with all variables and s
 
 ## F-tests generation -----------------------------------------------------------------------------------------------------------------------------------------
 
-regModels$`IMF loan approval`$df.residual <- degrees_of_freedom(
-  regModels$`IMF loan approval`
-)
-
-get_stars <- function(p){
-  if (p < 0.001) {
-    return('***')
-  } else if (p < 0.01) {
-    return('**')
-  } else if (p < 0.05) {
-    return('*')
-  } else if (p < 0.1) {
-    return('+')
-  } else {
-    return('')
-  }
-}
-
-glance_custom.tobit <- function(x, ...) {
-  p <- lht(x, test = 'F', 'us=eu')[2, 4]
-  data.frame(
-    'equality' = paste0(
-      '[',
-      round(p, 3),
-      get_stars(p),
-      ']'
-    ),
-    'periodFE' = if_else("year2000" %in% names(x$coefficients), "Yes",  ""),
-    'regression' = 'Tobit'
-  )
-}
-
-glance_custom.fixest <- function(x, ...) {
-  p <- lht(x, test = 'Chisq', 'us=eu')[2, 4]
-  data.frame(
-    'equality' = paste0(
-      '[',
-      round(p, 3),
-      get_stars(p),
-      ']'
-    ),
-    'periodFE' = if_else("year2000" %in% names(x$coefficients), "Yes",  ""),
-    'regression' = 'Probit'
-  )
-}
+source("helper/f_test.R")
 
 ## Variable renaming -------------------------------------------------------------------------------------------------------------------------------------------
 
