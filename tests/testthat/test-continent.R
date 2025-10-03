4d924e2ebe4b02fefd81b3b8edf161ae7cc7a759
Dianyi Yang <dianyi.yang@politics.ox.ac.uk>
2025-10-03 14:05:03 +0100

    test-continent: fix wd

--- tests/testthat/test-continent.R
+++ tests/testthat/test-continent.R
@@ -1,24 +1,31 @@
 library(testthat)
+library(here)
+i_am("tests/testthat/test-continent.R")
 
 test_that("Reproduce `data/panel_data.rds` (regenerate if needed)", {
   # Create temp workspace
   temp_dir <- withr::local_tempdir()
-  wd <- getwd()
-  
+
   # Copy inputs needed by continent.R
   dir.create(file.path(temp_dir, "data"), recursive = TRUE)
-  file.copy("data/panel_data_raw.dta", file.path(temp_dir, "data/panel_data_raw.dta"))
-  file.copy("data/continents.json", file.path(temp_dir, "data/continents.json"))
-  
+  file.copy(
+    here("data/panel_data_raw.dta"),
+    file.path(temp_dir, "data/panel_data_raw.dta")
+  )
+  file.copy(
+    here("data/continents.json"),
+    file.path(temp_dir, "data/continents.json")
+  )
+
   # Run continent.R in temp directory
   withr::with_dir(temp_dir, {
-    source(file.path(wd,"code/continent.R"))
+    source(here("code/continent.R"))
   })
-  
+
   # Load both versions
-  original <- readRDS("data/panel_data.rds")
+  original <- readRDS(here("data/panel_data.rds"))
   regenerated <- readRDS(file.path(temp_dir, "data/panel_data.rds"))
-  
+
   # Compare
   expect_identical(original, regenerated)
 })
